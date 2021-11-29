d50e75f7d805d32ad16f66cb63d723d17a627e2d
mudong <feng2r200@qq.com>
2021-11-29 12:07:37 +0800

    style(lualine): load extension for lualine.

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -20,6 +20,17 @@ function config.lualine()
             return ""
         end
     end
+    local symbols_outline = {
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'filetype'},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {'location'}
+        },
+        filetypes = {'Outline'}
+    }
 
     require("lualine").setup {
         options = {
@@ -34,8 +45,7 @@ function config.lualine()
             lualine_a = {"mode"},
             lualine_b = {{"branch"}, {"diff"}},
             lualine_c = {
-                {"lsp_progress"},
-                {gps_content, cond = gps.is_available}
+                {"lsp_progress"}, {gps_content, cond = gps.is_available}
             },
             lualine_x = {
                 {
@@ -60,7 +70,9 @@ function config.lualine()
             lualine_z = {}
         },
         tabline = {},
-        extensions = {}
+        extensions = {
+            "quickfix", "nvim-tree", "toggleterm", "fugitive", symbols_outline
+        }
     }
 end
 
