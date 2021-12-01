e06c46726e740f42389b00bf40f04eebc0157ea5
mudong <feng2r200@qq.com>
2021-12-01 12:19:25 +0800

    fix(lsp): remove lsp log level.

--- lua/modules/completion/lspconfig.lua
+++ lua/modules/completion/lspconfig.lua
@@ -12,7 +12,7 @@ end
 
 local lsp_installer = require("nvim-lsp-installer")
 
-vim.lsp.set_log_level("info")
+-- vim.lsp.set_log_level("info")
 
 lsp_installer.settings {
     log_level = vim.log.levels.DEBUG,
