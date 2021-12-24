dc3357def4de3245338f33c018c2d7eee60eb1c7
mudong <feng2r200@qq.com>
2021-12-24 13:52:07 +0800

    fix(plugin): change config in telescope.

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -7,32 +7,8 @@ function config.telescope()
         vim.cmd [[packadd popup.nvim]]
     end
 
-    if not packer_plugins["telescope-fzf-native.nvim"].loaded then
-        vim.cmd [[packadd telescope-fzf-native.nvim]]
-    end
-
-    if not packer_plugins["telescope-project.nvim"].loaded then
-        vim.cmd [[packadd telescope-project.nvim]]
-    end
-
     if not packer_plugins["sqlite.lua"].loaded then
         vim.cmd [[packadd sqlite.lua]]
-    end
-
-    if not packer_plugins["telescope-frecency.nvim"].loaded then
-        vim.cmd [[packadd telescope-frecency.nvim]]
-    end
-
-    if not packer_plugins["telescope-media-files.nvim"].loaded then
-        vim.cmd [[packadd telescope-media-files.nvim]]
-    end
-
-    if not packer_plugins["telescope-zoxide"].loaded then
-        vim.cmd [[packadd telescope-zoxide]]
-    end
-
-    if not packer_plugins["telescope-dap.nvim"].loaded then
-        vim.cmd [[packadd telescope-dap.nvim]]
     end
 
     require("telescope").setup {
@@ -88,12 +64,6 @@ function config.telescope()
         }
     }
 
-    require("telescope").load_extension("fzf")
-    require("telescope").load_extension("project")
-    require("telescope").load_extension("frecency")
-    require("telescope").load_extension("media_files")
-    require("telescope").load_extension("zoxide")
-    require("telescope").load_extension("dap")
 end
 
 function config.sniprun()
--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -14,24 +14,36 @@ tools["nvim-telescope/telescope.nvim"] = {
 tools["nvim-telescope/telescope-fzf-native.nvim"] = {
     opt = true,
     run = "make",
-    after = "telescope.nvim"
+    after = "telescope.nvim",
+    config = function() require("telescope").load_extension("fzf") end
 }
-
 tools["nvim-telescope/telescope-project.nvim"] = {
     opt = true,
-    after = "telescope.nvim"
+    after = "telescope.nvim",
+    config = function() require("telescope").load_extension("project") end
 }
 tools["nvim-telescope/telescope-frecency.nvim"] = {
     opt = true,
     after = "telescope.nvim",
-    requires = {{"tami5/sqlite.lua", opt = true}}
+    requires = {{"tami5/sqlite.lua", opt = true}},
+    config = function() require("telescope").load_extension("frecency") end
+}
+tools["jvgrootveld/telescope-zoxide"] = {
+    opt = true,
+    after = "telescope.nvim",
+    config = function() require("telescope").load_extension("zoxide") end
 }
 tools["nvim-telescope/telescope-media-files.nvim"] = {
     opt = true,
-    after = "telescope.nvim"
+    after = "telescope.nvim",
+    config = function() require("telescope").load_extension("media_files") end
 }
-tools["jvgrootveld/telescope-zoxide"] = {opt = true, after = "telescope.nvim"}
-tools["nvim-telescope/telescope-dap.nvim"] = {opt = true, after = "telescope.nvim"}
+tools["nvim-telescope/telescope-dap.nvim"] = {
+    opt = true,
+    after = "telescope.nvim",
+    config = function() require("telescope").load_extension("dap") end
+}
+
 tools["thinca/vim-quickrun"] = {opt = true, cmd = {"QuickRun", "Q"}}
 tools["michaelb/sniprun"] = {
     opt = true,
