610cf9f8c8d4ebabb4a0b26a3e0aee114b63c54e
mudong <feng2r200@users.noreply.github.com>
2022-02-09 13:40:42 +0800

    feat(plugin): re use FTerm to term.

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -87,11 +87,8 @@ editor["karb94/neoscroll.nvim"] = {
     config = conf.neoscroll
 }
 
-editor["akinsho/nvim-toggleterm.lua"] = {
-    opt = true,
-    event = "BufRead",
-    config = conf.toggleterm
-}
+editor["numtostr/FTerm.nvim"] = { opt = true, event = "BufRead" }
+
 editor["norcalli/nvim-colorizer.lua"] = {
     opt = true,
     event = "BufRead",
