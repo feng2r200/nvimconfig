4b3428989e7764d8546082241ff145472db1acc6
mudong <feng2r200@users.noreply.github.com>
2022-02-22 15:54:36 +0800

    fix: neovide config key map.

--- lua/gui/neovide.lua
+++ lua/gui/neovide.lua
@@ -1,16 +1,24 @@
 if vim.g.neovide then
+    local bind = require("kit.bind")
+    local map_cmd = bind.map_cmd
+    print("enter neovide")
+
     vim.cmd([[set guifont=JetBrainsMono\ Nerd\ Font:h12]])
     vim.g.neovide_refresh_rate=140
     vim.g.neovide_transparency=0.8
     vim.g.neovide_input_use_logo=true
     vim.g.neovide_cursor_antialiasing=false
 
-    vim.api.nvim_set_keymap("n", "∆", '<A-j>', {})
-    vim.api.nvim_set_keymap("n", "˚", '<A-k>', {})
-    vim.api.nvim_set_keymap("n", "¬", '<A-l>', {})
-    vim.api.nvim_set_keymap("n", "Ô", '<A-S-j>', {})
-    vim.api.nvim_set_keymap("n", "", '<A-S-k>', {})
-    vim.api.nvim_set_keymap("n", "¡", '<A-1>', {})
-    vim.api.nvim_set_keymap("n", "¶", '<A-7>', {})
+    local neovide_mapping = {
+        ["n|∆"] = map_cmd('<A-j>'),
+        ["n|˚"] = map_cmd('<A-k>'),
+        ["n|¬"] = map_cmd('<A-l>'),
+        ["n|Ô"] = map_cmd('<A-S-j>'),
+        ["n|"] = map_cmd('<A-S-k>'),
+        ["n|¡"] = map_cmd('<A-1>'),
+        ["n|¶"] = map_cmd('<A-7>'),
+    }
+
+    bind.nvim_load_mapping(neovide_mapping)
 end
 
