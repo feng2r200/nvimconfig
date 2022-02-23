30862781f4232f415b40aad0423fed99c08472ec
mudong <feng2r200@users.noreply.github.com>
2022-02-23 17:02:54 +0800

    fix: java use tab instead of space.

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -40,7 +40,7 @@ function autocmd.load_autocmds()
             {"FileType", "dashboard", "set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2",},
             {"BufNewFile,BufRead", "*.toml", " setf toml"},
             {"FileType", "make", "set noexpandtab shiftwidth=8 softtabstop=0"},
-            {"FileType", "java", "set noexpandtab tabstop=4, shiftwidth=4 softtabstop=4"},
+            {"FileType", "java", "set noexpandtab tabstop=4, shiftwidth=4, softtabstop=4"},
             {"FileType", "go,rust", "setlocal tabstop=4 shiftwidth=4"},
             {"FileType", "javascript,typescript,html", "setlocal tabstop=2 shiftwidth=2"},
             {"FileType", "*", [[setlocal formatoptions-=c formatoptions-=r formatoptions-=o]]},
