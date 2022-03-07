761da18c8ad3d0a858975b8e1f84c4fc87b95a7e
mudong <feng2r200@qq.com>
2022-03-07 11:01:32 +0800

    fix(telescope): Modify the configuration of telescope to suit my personal preferences.

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -13,27 +13,37 @@ function config.telescope()
 
     require("telescope").setup({
         defaults = {
-            prompt_prefix = "🔭 ",
+            prompt_prefix = "   ",
             selection_caret = " ",
+            sorting_strategy = "ascending",
+            layout_strategy = "horizontal",
             layout_config = {
-                horizontal = {prompt_position = "bottom", results_width = 0.6},
-                vertical = {mirror = false}
+                horizontal = {
+                    prompt_position = "top",
+                    preview_width = 0.55,
+                    results_width = 0.8,
+                },
+                vertical = {
+                    mirror = false,
+                },
+                width = 0.87,
+                height = 0.80,
+                preview_cutoff = 50,
             },
-            file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-            grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-            qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
             file_sorter = require("telescope.sorters").get_fuzzy_file,
-            file_ignore_patterns = {},
+            file_ignore_patterns = { "node_modules" },
             generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-            path_display = {"absolute"},
+            path_display = {"truncate"},
             winblend = 0,
             border = {},
-            borderchars = {
-                "─", "│", "─", "│", "╭", "╮", "╯", "╰"
-            },
+            borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
             color_devicons = true,
             use_less = true,
             set_env = {["COLORTERM"] = "truecolor"},
+            file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+            grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+            qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+            buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         },
         extensions = {
             fzf = {
