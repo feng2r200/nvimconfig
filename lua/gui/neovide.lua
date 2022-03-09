58d6fd86ad0b80ae99646330d04cd7cf9dacfd9e
mudong <feng2r200@qq.com>
2022-03-09 23:24:06 +0800

    fix: gui use nature keymap.

--- lua/gui/neovide.lua
+++ lua/gui/neovide.lua
@@ -1,21 +1,14 @@
-local bind = require("kit.bind")
-local map_cmd = bind.map_cmd
-
 vim.cmd([[set guifont=JetBrainsMono\ Nerd\ Font:h12]])
 vim.g.neovide_refresh_rate=140
 vim.g.neovide_transparency=0.8
 vim.g.neovide_input_use_logo=true
 vim.g.neovide_cursor_antialiasing=false
 
-local neovide_mapping = {
-    ["n|∆"] = map_cmd('<A-j>'),
-    ["n|˚"] = map_cmd('<A-k>'),
-    ["n|¬"] = map_cmd('<A-l>'),
-    ["n|Ô"] = map_cmd('<A-S-j>'),
-    ["n|"] = map_cmd('<A-S-k>'),
-    ["n|¡"] = map_cmd('<A-1>'),
-    ["n|¶"] = map_cmd('<A-7>'),
-}
-
-bind.nvim_load_mapping(neovide_mapping)
+vim.api.nvim_set_keymap("n", "∆", '<A-j>', {})
+vim.api.nvim_set_keymap("n", "˚", '<A-k>', {})
+vim.api.nvim_set_keymap("n", "¬", '<A-l>', {})
+vim.api.nvim_set_keymap("n", "Ô", '<A-S-j>', {})
+vim.api.nvim_set_keymap("n", "", '<A-S-k>', {})
+vim.api.nvim_set_keymap("n", "¡", '<A-1>', {})
+vim.api.nvim_set_keymap("n", "¶", '<A-7>', {})
 
