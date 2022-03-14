5da414bfffa2bd27cfefb194d7265cdf81b7c089
mudong <feng2r200@qq.com>
2022-03-14 14:31:26 +0800

    feat: simple path.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,49 +1,48 @@
 local M = {}
 
-local ui_conf = require("modules.ui.config")
+local conf = require("modules")
 
 M["kyazdani42/nvim-web-devicons"] = {opt = false}
-M["navarasu/onedark.nvim"] = {opt = false, config = ui_conf.onedark}
+M["navarasu/onedark.nvim"] = {opt = false, config = conf.ui.onedark}
 M["hoob3rt/lualine.nvim"] = {
     opt = true,
     after = "lualine-lsp-progress",
-    config = ui_conf.lualine,
+    config = conf.ui.lualine,
 }
 M["arkav/lualine-lsp-progress"] = {opt = true, after = "nvim-gps"}
 M[ "goolord/alpha-nvim"] = {
     opt = true,
     event = "BufWinEnter",
-    config = ui_conf.alpha,
+    config = conf.ui.alpha,
 }
 M["kyazdani42/nvim-tree.lua"] = {
     opt = true,
     cmd = {"NvimTreeToggle", "NvimTreeOpen"},
-    config = ui_conf.nvim_tree,
+    config = conf.ui.nvim_tree,
 }
 M["lewis6991/gitsigns.nvim"] = {
     opt = true,
     event = {"BufRead", "BufNewFile"},
-    config = ui_conf.gitsigns,
+    config = conf.ui.gitsigns,
 }
 M["lukas-reineke/indent-blankline.nvim"] = {
     opt = true,
     event = "BufRead",
-    config = ui_conf.indent_blankline
+    config = conf.ui.indent_blankline
 }
 M["akinsho/nvim-bufferline.lua"] = {
     opt = true,
     event = "BufRead",
-    config = ui_conf.nvim_bufferline
+    config = conf.ui.nvim_bufferline
 }
 
-local tool_conf = require("modules.tools.config")
 M["nvim-lua/plenary.nvim"] = {opt = false}
 M["nvim-lua/popup.nvim"] = {opt = false}
 M["nvim-telescope/telescope.nvim"] = {
     opt = true,
     module = "telescope",
     cmd = "Telescope",
-    config = tool_conf.telescope,
+    config = conf.tool.telescope,
 }
 M["nvim-telescope/telescope-fzf-native.nvim"] = { opt = true, run = "make", after = "telescope.nvim", }
 M["nvim-telescope/telescope-file-browser.nvim"] = { opt = true, after = "telescope.nvim" }
@@ -59,26 +58,24 @@ M["folke/which-key.nvim"] = {
 M["folke/trouble.nvim"] = {
     opt = true,
     cmd = {"Trouble", "TroubleToggle", "TroubleRefresh"},
-    config = tool_conf.trouble
+    config = conf.tool.trouble
 }
 M["gelguy/wilder.nvim"] = {
     opt = true,
     event = "CmdlineEnter",
-    config = tool_conf.wilder,
+    config = conf.tool.wilder,
 }
 M["romgrk/fzy-lua-native"] = { opt = true, after = "wilder.nvim"}
 M["nathom/filetype.nvim"] = {
-	opt = false,
-	config = tool_conf.filetype,
+    opt = false,
+    config = conf.tool.filetype,
 }
-
-local editor_conf = require("modules.editor.config")
 
 M["junegunn/vim-easy-align"] = {opt = true, cmd = "EasyAlign"}
 M["itchyny/vim-cursorword"] = {
     opt = true,
     event = {"BufReadPre", "BufNewFile"},
-    config = editor_conf.vim_cursorwod
+    config = conf.editor.vim_cursorwod
 }
 M["terrortylor/nvim-comment"] = {
     opt = true,
@@ -94,7 +91,7 @@ M["nvim-treesitter/nvim-treesitter"] = {
     opt = true,
     run = ":TSUpdate",
     event = "BufRead",
-    config = editor_conf.nvim_treesitter
+    config = conf.editor.nvim_treesitter
 }
 M["nvim-treesitter/nvim-treesitter-textobjects"] = {
     opt = true,
@@ -120,18 +117,18 @@ M["mfussenegger/nvim-ts-hint-textobject"] = {
 M["SmiteshP/nvim-gps"] = {
     opt = true,
     after = "nvim-treesitter",
-    config = editor_conf.nvim_gps
+    config = conf.editor.nvim_gps
 }
 M["sbdchd/neoformat"] = {opt = true, cmd = "Neoformat"}
 M["windwp/nvim-ts-autotag"] = {
     opt = true,
     ft = {"html", "xml"},
-    config = editor_conf.autotag
+    config = conf.editor.autotag
 }
 M["andymass/vim-matchup"] = {
     opt = true,
     after = "nvim-treesitter",
-    config = editor_conf.matchup
+    config = conf.editor.matchup
 }
 M["romainl/vim-cool"] = {
     opt = true,
@@ -156,25 +153,25 @@ M['rlane/pounce.nvim'] = {
 M["karb94/neoscroll.nvim"] = {
     opt = true,
     event = "WinScrolled",
-    config = editor_conf.neoscroll
+    config = conf.editor.neoscroll
 }
 M["akinsho/nvim-toggleterm.lua"] = {
     opt = true,
     event = "BufRead",
-    config = editor_conf.toggleterm
+    config = conf.editor.toggleterm
 }
 M["norcalli/nvim-colorizer.lua"] = {
     opt = true,
     event = "BufRead",
-    config = editor_conf.nvim_colorizer
+    config = conf.editor.nvim_colorizer
 }
 M["mfussenegger/nvim-dap"] = {
     opt = false,
-    config = editor_conf.dap
+    config = conf.editor.dap
 }
 M["rcarriga/nvim-dap-ui"] = {
     opt = true,
-    config = editor_conf.dapui,
+    config = conf.editor.dapui,
 }
 M["theHamsta/nvim-dap-virtual-text"] = {
     opt = true,
@@ -190,20 +187,18 @@ M["famiu/bufdelete.nvim"] = {
 M["chentau/marks.nvim"] = {
     opt = true,
     event = "BufReadPost",
-    config = editor_conf.marks
+    config = conf.editor.marks
 }
 M["rlue/vim-barbaric"] = { opt = false }
 M["christoomey/vim-tmux-navigator"] = {
     opt = false,
-    config = editor_conf.tmuxnavigator
+    config = conf.editor.tmuxnavigator
 }
-
-local completion_conf = require("modules.completion.config")
 
 M["neovim/nvim-lspconfig"] = {
     opt = true,
     event = "BufReadPre",
-    config = completion_conf.nvim_lsp
+    config = conf.completion.nvim_lsp
 }
 M["williamboman/nvim-lsp-installer"] = {
     opt = true,
@@ -212,11 +207,11 @@ M["williamboman/nvim-lsp-installer"] = {
 M["stevearc/aerial.nvim"] = {
     opt = true,
     after = "nvim-lspconfig",
-    config = completion_conf.aerial,
+    config = conf.completion.aerial,
 }
 M["ray-x/lsp_signature.nvim"] = {opt = true, after = "nvim-lspconfig"}
 M["hrsh7th/nvim-cmp"] = {
-    config = completion_conf.cmp,
+    config = conf.completion.cmp,
     event = "InsertEnter",
     requires = {
         {"lukas-reineke/cmp-under-comparator"},
@@ -230,18 +225,18 @@ M["hrsh7th/nvim-cmp"] = {
 }
 M["L3MON4D3/LuaSnip"] = {
     after = "nvim-cmp",
-    config = completion_conf.luasnip,
+    config = conf.completion.luasnip,
     requires = "rafamadriz/friendly-snippets"
 }
 M["windwp/nvim-autopairs"] = {
     after = "nvim-cmp",
-    config = completion_conf.autopairs
+    config = conf.completion.autopairs
 }
 
 M["simrat39/rust-tools.nvim"] = {
     opt = true,
     ft = "rust",
-    config = require("modules.lang.config").rust_tools,
+    config = require("modules.lang").rust_tools,
 }
 M["mfussenegger/nvim-jdtls"] = {
     opt = true,
---
+++ lua/modules/init.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.ui = require("modules.ui")
+M.tool = require("modules.tools")
+M.editor = require("modules.editor")
+M.completion = require("modules.completion")
+
+return M
