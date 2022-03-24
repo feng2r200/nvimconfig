9de34c1fe151d7df4356db48cd6726992bea5124
mudong <feng2r200@qq.com>
2022-03-24 18:27:12 +0800

    fix(keymap): use lsp code_action instead of saga or telescope to use jdtls code action.

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -63,8 +63,8 @@ local plug_map = {
     ["n|gs"]          = map_cr("Lspsaga signature_help"):with_noremap():with_silent(),
     ["n|gr"]          = map_cr("Lspsaga rename"):with_noremap():with_silent(),
 
-    ["n|<leader>ga"]  = map_cr("lua require('telescope.builtin').lsp_code_actions{}"):with_noremap():with_silent(),
-    ["v|<leader>ga"]  = map_cr("lua require('telescope.builtin').lsp_range_code_actions{}"):with_noremap():with_silent(),
+    ["n|<leader>ca"]  = map_cr("lua vim.lsp.buf.code_action()"):with_noremap():with_silent(),
+    ["v|<leader>ca"]  = map_cr("lua vim.lsp.buf.range_code_action()"):with_noremap():with_silent(),
 
     ["n|<leader>gi"]  = map_cr("lua vim.lsp.buf.incoming_calls()"):with_noremap():with_silent(),
     ["n|<leader>go"]  = map_cr("lua vim.lsp.buf.outgoing_calls()"):with_noremap():with_silent(),
