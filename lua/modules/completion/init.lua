8c1ca29504d9bf41c0863ae2cef09689711dc422
mudong <feng2r200@qq.com>
2022-03-31 14:21:12 +0800

    fix: fix cmp sort.

--- lua/modules/completion/init.lua
+++ lua/modules/completion/init.lua
@@ -248,7 +248,7 @@ function config.cmp()
             comparators = {
                 cmp.config.compare.offset, cmp.config.compare.exact,
                 cmp.config.compare.score, require("cmp-under-comparator").under,
-                cmp.recently_used, cmp.config.compare.kind,
+                cmp.config.compare.recently_used, cmp.config.compare.kind,
                 cmp.config.compare.sort_text, cmp.config.compare.length,
                 cmp.config.compare.order
             }
