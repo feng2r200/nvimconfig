0a09aa9bc55caaafd339a31f48c201544c743d2b
mudong <feng2r200@qq.com>
2022-04-22 10:52:31 +0800

    fix: update jdk 11.

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -6,7 +6,7 @@ vim.cmd [[packadd nvim-jdtls]]
 
 local java_path = {
     ["8"] = "/usr/local/Cellar/openjdk@8/1.8.0+322/libexec/openjdk.jdk/Contents/Home",
-    ["11"]= "/usr/local/Cellar/openjdk@11/11.0.14.1/libexec/openjdk.jdk/Contents/Home",
+    ["11"]= "/usr/local/Cellar/openjdk@11/11.0.15/libexec/openjdk.jdk/Contents/Home",
 }
 
 local get_cmd = function()
