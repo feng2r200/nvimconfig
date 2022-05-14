9d0e6cdc036f3f898a71a201499e3c3ff96ea0cd
mudong <feng2r200@qq.com>
2022-05-14 14:27:32 +0800

    fix: remove filetype options.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -115,7 +115,6 @@ local function load_options()
         },
         python_host_prog = "/usr/bin/python",
         python3_host_prog = "/usr/local/bin/python3",
-        did_load_filetypes = 1,
         did_load_fzf = 1,
         did_load_gtags = 1,
         did_load_gzip = 1,
