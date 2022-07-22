e72abf512806d356e6cce6760891f96dc172ff46
mudong <feng2r200@qq.com>
2022-07-22 09:21:23 +0800

    fix: Repair the signature that blocks the line of sight

--- lua/configs/signature.lua
+++ lua/configs/signature.lua
@@ -16,12 +16,12 @@ signature.setup({
 
   floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
-  floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+  floating_window_above_cur_line = false, -- try to place the floating above the current line when possible Note:
   -- will set to true when fully tested, set to false will use whichever side has more space
   -- this setting will be helpful if you do not want the PUM and floating win overlap
 
-  floating_window_off_x = 2, -- adjust float windows x position.
-  floating_window_off_y = 10, -- adjust float windows y position.
+  floating_window_off_x = -2, -- adjust float windows x position.
+  floating_window_off_y = -5, -- adjust float windows y position.
 
   fix_pos = true,  -- set to true, the floating window will not auto-close until finish all parameters
   hint_enable = true, -- virtual hint enable
@@ -32,18 +32,18 @@ signature.setup({
   -- to view the hiding contents
   max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
   handler_opts = {
-    border = "rounded"   -- double, rounded, single, shadow, none
+    border = "none"   -- double, rounded, single, shadow, none
   },
 
   always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
 
   auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
   extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-  zindex = 10, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+  zindex = 1, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
 
   padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
 
-  transparency = 20, -- disabled by default, allow floating win transparent value 1~100
+  transparency = 10, -- disabled by default, allow floating win transparent value 1~100
   shadow_blend = 36, -- if you using shadow as border use this set the opacity
   shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
   timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
