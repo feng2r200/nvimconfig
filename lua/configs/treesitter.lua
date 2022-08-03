0fe2e4996f6e56ea28d911d78eb6380716f95ed6
mudong <feng2r200@qq.com>
2022-08-03 10:48:50 +0800

    update: treesitter settings.

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -11,58 +11,93 @@ treesitter.setup({
   },
   sync_install = false,
   ignore_install = {},
+  matchup = {
+    enable = true,
+    disable_virtual_text = false,
+    disable = {},
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
+    disable = {},
+  },
+  autopairs = { enable = true },
+  indent = {
+    enable = true,
+    disable = {},
   },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
   },
+  autotag = {
+    enable = true,
+    disable = {},
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+    disable = {},
+  },
   textobjects = {
-    swap = {
-      enable = false,
-    },
     select = {
       enable = true,
       lookahead = true,
       keymaps = {
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
+        ["at"] = "@class.outer",
+        ["it"] = "@class.inner",
+        ["ac"] = "@call.outer",
+        ["ic"] = "@call.inner",
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+        ["ai"] = "@conditional.outer",
+        ["ii"] = "@conditional.inner",
+        ["a/"] = "@comment.outer",
+        ["i/"] = "@comment.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+        ["as"] = "@statement.outer",
+        ["is"] = "@scopename.inner",
+        ["aA"] = "@attribute.outer",
+        ["iA"] = "@attribute.inner",
+        ["aF"] = "@frame.outer",
+        ["iF"] = "@frame.inner",
       },
     },
     move = {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-          ["]["] = "@function.outer",
-          ["]m"] = "@class.outer",
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
       },
       goto_next_end = {
-          ["]]"] = "@function.outer",
-          ["]M"] = "@class.outer",
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
       },
       goto_previous_start = {
-          ["[["] = "@function.outer",
-          ["[m"] = "@class.outer",
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
       },
       goto_previous_end = {
-          ["[]"] = "@function.outer",
-          ["[M"] = "@class.outer",
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<Space>."] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<Space>,"] = "@parameter.inner",
       },
     },
   },
-  autopairs = { enable = true },
-  autotag = { enable = true },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  },
-  incremental_selection = { enable = true },
-  indent = { enable = false },
-  matchup = { enable = true },
 })
 
