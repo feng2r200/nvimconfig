9eacffcd0e84dbf90e21356f2087e702c8a1ea45
mudong <feng2r200@qq.com>
2022-09-09 09:27:54 +0800

    fix: signature config.

--- lua/configs/lsp/handlers.lua
+++ lua/configs/lsp/handlers.lua
@@ -78,14 +78,14 @@ local function attach_signature(client, bufnr)
     verbose = false, -- show debug line number
 
     bind = true, -- This is mandatory, otherwise border config won't get registered.
-    doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+    doc_lines = 18, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
     -- set to 0 if you DO NOT want any API comments be shown
     -- This setting only take effect in insert mode, it does not affect signature help in normal
     -- mode, 10 by default
 
     floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
-    floating_window_above_cur_line = false, -- try to place the floating above the current line when possible Note:
+    floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
     -- will set to true when fully tested, set to false will use whichever side has more space
     -- this setting will be helpful if you do not want the PUM and floating win overlap
 
@@ -97,11 +97,11 @@ local function attach_signature(client, bufnr)
     hint_prefix = "🐼 ",  -- Panda for parameter
     hint_scheme = "String",
     hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-    max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+    max_height = 18, -- max height of signature floating_window, if content is more than max_height, you can scroll down
     -- to view the hiding contents
-    max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+    max_width = 117, -- max_width of signature floating_window, line will be wrapped if exceed max_width
     handler_opts = {
-      border = "double"   -- double, rounded, single, shadow, none
+      border = "shadow"   -- double, rounded, single, shadow, none
     },
 
     always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
