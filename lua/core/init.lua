34157a1fca5f556d82adec23766fc93fdf1ba278
mudong <feng2r200@qq.com>
2022-10-19 17:31:01 +0800

    feat: create core.init

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,14 +1,11 @@
-if vim.g.vscode then
-  return
-end
-
 for _, source in ipairs {
-  "bootstrap",
-  "core",
+  "core.plugins",
+  "core.mappings",
+  "core.autocmds",
+  "core.options",
 } do
   local status_ok, fault = pcall(require, source)
   if not status_ok then
     vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
   end
 end
-
