73b17c5060542c76e8b7fab76e1e0b01628b5912
mudong <feng2r200@qq.com>
2022-10-19 17:37:04 +0800

    feat: mason unload catch

--- lua/bootstrap/init.lua
+++ lua/bootstrap/init.lua
@@ -1,11 +1,15 @@
 local impatient_ok, _ = pcall(require, "impatient")
 if not impatient_ok then
-  print("The plugin [impatient] not found. Please run :PackerSync!")
+  vim.notify("The plugin [impatient] not found. Please run :PackerSync!")
 end
 
 local nvim_config_dir = vim.fn.stdpath("config")
 local nvim_bin_dir = nvim_config_dir .. "/bin"
 
-local platform = require('mason-core.platform')
+local status_ok, platform = pcall(require, 'mason-core.platform')
+if not status_ok then
+  vim.notify("Mason not loaded.")
+end
+
 vim.env.PATH = nvim_bin_dir .. platform.path_sep .. vim.env.PATH
 
