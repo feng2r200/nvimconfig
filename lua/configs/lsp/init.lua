9d8586a1ead6657b1dff6e7046abd50be6453f90
mudong <feng2r200@qq.com>
2022-10-24 15:02:45 +0800

    feat: change lsp config

--- lua/configs/lsp/init.lua
+++ lua/configs/lsp/init.lua
@@ -2,6 +2,7 @@ M = {}
 
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
+  vim.notify("lsp - lspconfig not loaded")
   return
 end
 
--- lua/configs/nvim-mason.lua
+++ lua/configs/nvim-mason.lua
@@ -9,25 +9,15 @@ if not status_ok_1 then
 end
 
 local servers = {
-  "cssls",
-  "cssmodules_ls",
-  "emmet_ls",
-  "html",
   "jdtls",
   "jsonls",
-  "solc",
   "sumneko_lua",
   "sqls",
-  "tflint",
-  "terraformls",
-  "tsserver",
   "pyright",
   "yamlls",
   "bashls",
   "clangd",
   "rust_analyzer",
-  "taplo",
-  "lemminx"
 }
 
 local settings = {
