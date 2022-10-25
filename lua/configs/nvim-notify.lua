2106a5e07baa95f4c5ce628ef1a87f9b39bfec96
mudong <feng2r200@qq.com>
2022-10-25 14:44:16 +0800

    fix: change notify - timeout

--- lua/configs/nvim-notify.lua
+++ lua/configs/nvim-notify.lua
@@ -18,7 +18,7 @@ vim.notify.setup({
   render = "default",
 
   -- Default timeout for notifications
-  timeout = 3000,
+  timeout = 1000,
 
   -- For stages that change opacity this is treated as the highlight behind the window
   -- Set this to either a highlight group, an RGB hex value e.g. "#000000" or a function returning an RGB code for dynamic values
