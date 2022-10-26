91d630aae9d1d3ccc6f4d8430734549815b874e1
mudong <feng2r200@qq.com>
2022-10-26 21:19:18 +0800

    feat: remove rust unwrap reminder

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -65,32 +65,3 @@ null_ls.setup {
   },
 }
 
-local unwrap = {
-  method = null_ls.methods.DIAGNOSTICS,
-  filetypes = { "rust" },
-  generator = {
-    fn = function(params)
-      local diagnostic = {}
-      -- sources have access to a params object
-      -- containing info about the current file and editor state
-      for i, line in ipairs(params.content) do
-        local col, end_col = line:find "unwrap()"
-        if col and end_col then
-          -- null-ls fills in undefined positions
-          -- and converts source diagnostics into the required format
-          table.insert(diagnostic, {
-            row = i,
-            col = col,
-            end_col = end_col,
-            source = "unwrap",
-            message = "hey " .. os.getenv "USER" .. ", don't forget to handle this",
-            severity = 2,
-          })
-        end
-      end
-      return diagnostic
-    end,
-  },
-}
-
-null_ls.register(unwrap)
