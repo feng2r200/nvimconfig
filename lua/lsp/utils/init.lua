cea40d9e021c91945169fb2832ca75d44ffac931
mudong <feng2r200@qq.com>
2022-11-09 15:29:57 +0800

    feat: default format use null-ls

--- lua/lsp/utils/init.lua
+++ lua/lsp/utils/init.lua
@@ -28,53 +28,12 @@ end
 local format_lsp_mapping = {}
 format_lsp_mapping["java"] = "jdtls"
 
--- sql_formatter
-format_lsp_mapping["sql"] = "null-ls"
-format_lsp_mapping["mysql"] = "null-ls"
--- prettier
-format_lsp_mapping["javascript"] = "null-ls"
-format_lsp_mapping["javascriptreact"] = "null-ls"
-format_lsp_mapping["typescript"] = "null-ls"
-format_lsp_mapping["typescriptreact"] = "null-ls"
-format_lsp_mapping["vue"] = "null-ls"
-format_lsp_mapping["css"] = "null-ls"
-format_lsp_mapping["scss"] = "null-ls"
-format_lsp_mapping["less"] = "null-ls"
-format_lsp_mapping["html"] = "null-ls"
-format_lsp_mapping["json"] = "null-ls"
-format_lsp_mapping["jsonc"] = "null-ls"
-format_lsp_mapping["yaml"] = "null-ls"
-format_lsp_mapping["markdown"] = "null-ls"
-format_lsp_mapping["graphql"] = "null-ls"
-format_lsp_mapping["handlebars"] = "null-ls"
-format_lsp_mapping["nginx"] = "null-ls"
-
--- xmllint
-format_lsp_mapping["xml"] = "null-ls"
-
--- taplo
-format_lsp_mapping["toml"] = "null-ls"
-
--- shfmt
-format_lsp_mapping["sh"] = "null-ls"
--- stylua
-format_lsp_mapping["lua"] = "null-ls"
-
--- rustfmt
-format_lsp_mapping["rust"] = "null-ls"
-
-format_lsp_mapping["http"] = "null-ls"
-
--- gofmt
-format_lsp_mapping["go"] = "null-ls"
-
--- clang_format
 format_lsp_mapping["c"] = "clangd"
 format_lsp_mapping["cpp"] = "clangd"
 
 M.filter_format_lsp_client = function(client, bufnr)
   local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-  local cn = format_lsp_mapping[filetype]
+  local cn = format_lsp_mapping[filetype] or "null-ls"
   return client.name == cn
 end
 
