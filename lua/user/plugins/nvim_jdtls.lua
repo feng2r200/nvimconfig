70ee164bec0b703819cec463665abd926e17d6e8
mudong <feng2r200@qq.com>
2023-01-08 21:46:17 +0800

    fix: java lsp load fail

--- lua/user/plugins/nvim_jdtls.lua
+++ lua/user/plugins/nvim_jdtls.lua
@@ -1,7 +1,7 @@
 local M = { "mfussenegger/nvim-jdtls", ft = "java" }
 
 M.config = function()
-  require("user.lsp.settings.java").init()
+  require("user.lsp.languages.java").init()
 end
 
 return M
