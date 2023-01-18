0ea9343403bcd3c0e7f9ff5a4ad841ed813fcc40
mudong <feng2r200@qq.com>
2023-01-18 12:00:29 +0800

    fix: markdown colorcolumn to 100

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,4 +1,5 @@
 vim.cmd [[setlocal conceallevel=2]]
+vim.cmd [[setlocal colorcolumn=100]]
 
 vim.keymap.set("n", "<F12>", "<cmd>MarkdownPreviewToggle<cr>", { silent = true, desc = "Markdown preview" })
 
