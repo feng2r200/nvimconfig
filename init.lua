47bc1812557dda6f11d37c8d82a69efdaf1d5d3d
liangdong <feng2r200@qq.com>
2023-07-25 21:09:17 +0800

    feat: mv pack content to init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,30 @@ if vim.g.vscode then
   return
 end
 
-require "user.pack"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.delete(lazypath, "rf")
+    vim.fn.system {
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+    }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup {
+    spec = "user.plugins",
+    defaults = {
+        lazy = false,
+        version = "*",
+    },
+    install = { colorscheme = { "navarasu/onedark.nvim" } },
+    checker = { enabled = false, notify = false },
+}
+
 require "user.globals"
 require "user.options"
 require "user.mappings"
