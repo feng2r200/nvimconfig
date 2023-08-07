1c089ad893e92b61ee02ba2836e896f794a329cc
liangdong <feng2r200@qq.com>
2023-08-07 10:02:30 +0800

    feat: add window-picker

--- lua/user/resources/util.lua
+++ lua/user/resources/util.lua
@@ -71,4 +71,14 @@ return {
       },
     },
   },
+
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
 }
