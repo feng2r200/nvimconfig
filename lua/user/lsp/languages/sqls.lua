2389c5c047ce27e74960f755f5bbd85268dbb851
liangdong <feng2r200@qq.com>
2023-08-11 18:51:44 +0800

    feat: lsp config

--- lua/user/lsp/languages/sqls.lua
+++
@@ -1,7 +0,0 @@
-return {
-  cmd = { "sql-language-server", "up", "--method", "stdio" },
-  filetypes = { "sql", "mysql" },
-  root_dir = function()
-    return os.getenv("HOME") .. "/.config/sql-language-server/"
-  end
-}
