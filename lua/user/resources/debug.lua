57fb82390b751ea79e4d265217ffdd0f465066b7
liangdong <feng2r200@qq.com>
2023-08-11 11:35:20 +0800

    feat: move dap -> resource/dap

--- lua/user/resources/debug.lua
+++ lua/user/resources/debug.lua
@@ -141,10 +141,10 @@ M.config = function()
   dap.defaults.fallback.force_external_terminal = true
 
   for _, dap_opt in ipairs {
-    "user.dap.python",
-    "user.dap.go",
-    "user.dap.cpp",
-    "user.dap.java",
+    "user.resources.dap.python",
+    "user.resources.dap.go",
+    "user.resources.dap.cpp",
+    "user.resources.dap.java",
   } do
     local opt_status_ok, fault = pcall(require, dap_opt)
     if not opt_status_ok then
