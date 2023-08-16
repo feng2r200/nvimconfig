9db0bf4dfcf0c09981b1b80517655ec08ae4a59b
liangdong <feng2r200@qq.com>
2023-08-16 10:30:09 +0800

    feat: change color schema

--- lua/user/resources/colorscheme.lua
+++ lua/user/resources/colorscheme.lua
@@ -4,18 +4,11 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      local c = require("vscode.colors").get_colors()
       require("vscode").setup {
         style = 'dark',
         transparent = true,
         italic_comments = true,
         disable_nvimtree_bg = true,
-        color_overrides = {
-          vscLineNumber = "#FFFFFF",
-        },
-        group_overrides = {
-          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-        },
       }
       require("vscode").load()
     end,
