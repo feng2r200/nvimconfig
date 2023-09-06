2e3ecbb9095d9d6bbdad3ab3d1d15e2501af0849
liangdong <feng2r200@qq.com>
2023-09-06 10:32:42 +0800

    feat: remove mode x <C-j><C-k>

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -7,8 +7,5 @@ map("", "<C-t>", "<Nop>", {})
 map("v", "<", "<gv", default_options)
 map("v", ">", ">gv", default_options)
 
-map("x", "<C-j>", ":m'>+1<cr>gv-gv", default_options)
-map("x", "<C-k>", ":m'<-2<cr>gv-gv", default_options)
-
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" } )
 
