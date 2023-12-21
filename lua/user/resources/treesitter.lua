037c8d802ba23b6b2f05098571a28758df7e1860
liangdong <feng2r200@qq.com>
2023-12-21 11:51:11 +0800

    fix: matchup not use in sh

--- lua/user/resources/treesitter.lua
+++ lua/user/resources/treesitter.lua
@@ -42,11 +42,11 @@ return {
       },
       pairs = {
         enable = true,
-        disable = {},
+        disable = {"sh", "bash"},
         highlight_pair_events = {}, -- e.g. {"CursorMoved"}, -- when to highlight the pairs, use {} to deactivate highlighting
         highlight_self = false, -- whether to highlight also the part of the pair under cursor (or only the partner)
         goto_right_end = false, -- whether to go to the end of the right partner or the beginning
-        fallback_cmd_normal = "call matchit#Match_wrapper('',1,'n')", -- What command to issue when we can't find a pair (e.g. "normal! %")
+        -- fallback_cmd_normal = "call matchit#Match_wrapper('',1,'n')", -- What command to issue when we can't find a pair (e.g. "normal! %")
         keymaps = {
           goto_partner = nil,
           delete_balanced = nil,
