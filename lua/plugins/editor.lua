12936234c19956a0a13cfb17af80ff25a93f6d10
mudong <feng2r200@qq.com>
2024-03-04 14:55:54 +0800

    feat: change gitsign to always show current_line_blame

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -56,4 +56,17 @@ return {
       show_modified = false,
     },
   },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        ignore_whitespace = true,
+        delay = 300,
+      },
+    },
+  },
 }
