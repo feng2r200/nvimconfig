26064977f898f8bf29ba9bff1c1f1cb0f1f15d5d
mudong <feng2r200@qq.com>
2024-04-07 09:44:01 +0800

    feat: remove cmp-tmux

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -3,11 +3,11 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "lukas-reineke/cmp-rg",
-      "andersevenrud/cmp-tmux",
+      -- "andersevenrud/cmp-tmux",
     },
     opts = function(_, opts)
       table.insert(opts.sources, { name = "rg", group_index = 3 })
-      table.insert(opts.sources, { name = "tmux", group_index = 3 })
+      -- table.insert(opts.sources, { name = "tmux", group_index = 3 })
 
       local cmp = require("cmp")
       local luasnip = require("luasnip")
