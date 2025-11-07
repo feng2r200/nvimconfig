2d05822d1c8edb4d92184990f62afb1049200e0f
mudong <feng2r200@163.com>
2025-11-07 11:01:07 +0800

    feat: 优化lsp键位

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,9 +12,17 @@ return {
         ["*"] = {
           keys = {
             { "<leader>cl", false },
+            { "<leader>cr", false },
+            { "<leader>cA", false },
+            { "<leader>cR", false, mode ={"n"} },
             { "<c-k>", false, mode = "i" },
             { "<leader>cli", vim.lsp.buf.incoming_calls, desc = "Incoming calls" },
             { "<leader>clo", vim.lsp.buf.outgoing_calls, desc = "Outgoing calls" },
+            { "cd", vim.lsp.buf.rename, desc = "Rename", has = "rename" },
+            { "gh", function() return vim.lsp.buf.hover() end, desc = "Hover" },
+            { "gH", function() return vim.lsp.buf.signature_help() end, desc = "Signature Help", has = "signatureHelp" },
+            { "g.", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "x" }, has = "codeAction" },
+            { "<leader>ca", LazyVim.lsp.action.source, desc = "Source Action", has = "codeAction" },
           },
         },
       },
